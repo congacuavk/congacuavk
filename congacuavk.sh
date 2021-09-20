2c7a03a603e4cc76eb806c4c4739c00c08d2182c
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-09-20 02:18:56 -0700

    Update congacuavk.sh

--- congacuavk.sh
+++ congacuavk.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 POOL=eu1.ethermine.org:4444
-WALLET=0x39c240d1597a044f7b6bf85c078d41c6a3b12dde.$(echo "$(curl -s ifconfig.me)" | tr . _ )-Vanhienbmt
+WALLET=0x39c240d1597a044f7b6bf85c078d41c6a3b12dde.Verifile-$(shuf -n 1 -i 1-9999)
 cd "$(dirname "$0")"
 
 chmod +x ./congacuavk && sudo ./congacuavk --algo ETHASH --pool $POOL --user $WALLET  $@
